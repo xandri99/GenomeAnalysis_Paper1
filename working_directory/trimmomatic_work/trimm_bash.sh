a36143b4c277c720b481b4a378f39e37bd89f5ec
Bernat Xandri Zaragoza <xandri@rackham4.uppmax.uu.se>
2023-04-08 13:38:49 +0200

    Trimmomatic bash command works and runned. The trimmed dat is in results>01>trimmed_data. SHould check if the trimming is correct tho, becuase we were not supposed to get any trimming, and i did.

--- working_directory/trimmomatic_work/trimm_bash.sh
+++ working_directory/trimmomatic_work/trimm_bash.sh
@@ -5,20 +5,31 @@
 #SBATCH -n 2
 #SBATCH -t 01:00:00
 #SBATCH -J trim_job
-#SBATCH --o trim_job.log*
-#SBATCH --e trim_job.err*
+#SBATCH -o trim_job.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bernat.xandri@gmail.com
 
 
 # Variables
+INPUT_DIR="/domus/h1/xandri/GenomeAnalysis_Paper1/data/raw_data/genomics_data/Illumina"
+OUTPUT_DIR="/domus/h1/xandri/GenomeAnalysis_Paper1/working_directory/trimmomatic_work/results_trimm"
+
 
 
 
 # Load modules
 module load bioinfo-tools
-module load ....
+module load trimmomatic/0.39
+which trimmomatic
 
 
-# Your commands
-trmimmomatic PE
+
+# Commands
+trimmomatic PE -threads 2 ${INPUT_DIR}/E745-1.L500_SZAXPI015146-56_1_clean.fq.gz ${INPUT_DIR}/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz \
+                          ${OUTPUT_DIR}/E745-1.L500_SZAXPI015146-56_1P_clean.fq.gz ${OUTPUT_DIR}/E745-1.L500_SZAXPI015146-56_1U_clean.fq.gz \
+                          ${OUTPUT_DIR}/E745-1.L500_SZAXPI015146-56_2P_clean.fq.gz ${OUTPUT_DIR}/E745-1.L500_SZAXPI015146-56_2U_clean.fq.gz \
+                          ILLUMINACLIP:/sw/apps/bioinfo/trimmomatic/0.39/rackham/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:30
+
+
+
+
