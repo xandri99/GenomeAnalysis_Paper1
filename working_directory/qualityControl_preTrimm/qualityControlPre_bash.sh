419900691aaf9029ffa40bfb0f5030dc950a8baf
Bernat Xandri Zaragoza <xandri@rackham4.uppmax.uu.se>
2023-04-08 12:41:12 +0200

    first QC done, with reults. Reframing of the data managment. Bash for trimming in process.

--- working_directory/qualityControl_preTrimm/qualityControlPre_bash.sh
+++ working_directory/qualityControl_preTrimm/qualityControlPre_bash.sh
@@ -4,24 +4,21 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 01:00:00
-#SBATCH -J QC_job
-#SBATCH --o QC_job.log*
-#SBATCH --e QC_job.err*
+#SBATCH -J QC_pre_job
+#SBATCH -o QC_pre_job.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bernat.xandri@gmail.com
 
 
 # Variables
-export INPUT_DIR = /domus/h1/xandri/GenomeAnalysis_Paper1/data/raw_data/genomics_data/Illumina
-
+INPUT_DIR="/domus/h1/xandri/GenomeAnalysis_Paper1/data/raw_data/genomics_data/Illumina"
+OUTPUT_DIR="/domus/h1/xandri/GenomeAnalysis_Paper1/working_directory/qualityControl_preTrimm/results_QCpre"
 
 
 # Load modules
 module load bioinfo-tools
-module load ....
+module load FastQC
 
 
 # Your commands
-trmimmomatic PE
-
-
+fastqc -o ${OUTPUT_DIR} ${INPUT_DIR}/*
