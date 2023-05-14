026c485fb839d1db50e8e3087c80287df9daf898
Bernat Xandri Zaragoza <xandri@rackham3.uppmax.uu.se>
2023-05-14 15:15:22 +0200

    Assembly evaluaion for both canu and spades

--- code/03_assembly_evaluation/quast_spades_ae.sh
+++ code/03_assembly_evaluation/quast_spades_ae.sh
@@ -4,15 +4,15 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 05:00:00
-#SBATCH -J quast_ae_Canu
-#SBATCH -o /home/xandri/GenomeAnalysis_Paper1/logs/assembly_evaluation_quast_canu_job.log
+#SBATCH -J quast_ae_Spades
+#SBATCH -o /home/xandri/GenomeAnalysis_Paper1/logs/assembly_evaluation_quast_Spades_job.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user bernat.xandri@gmail.com
 
 
 # Variables
 INPUT_DATA="/home/xandri/GenomeAnalysis_Paper1/analysis/02_genome_assembly/spades/Illumina/contigs.fasta"
-OUTPUT_DIR="/home/xandri/GenomeAnalysis_Paper1/analysis/03_assembly_evaluation/quast_canu_assembly_evaluation"
+OUTPUT_DIR="/home/xandri/GenomeAnalysis_Paper1/analysis/03_assembly_evaluation/quast_spades_assembly_evaluation"
 REFERENCE_GENOME="/domus/h1/xandri/GenomeAnalysis_Paper1/data/Enterococcus_faecium_reference_genome.fasta"
 
 # Load modules
